498b7d7bdde3880b7a5436b6f1ebc70e8baa26a7
Mleon7 <2710887576@qq.com>
2023-08-11 11:48:34 +0800

    lab01/ex10_ll_cycle.c

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,22 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    // chatgpt
+    if (head == NULL) {
+        return 0;  // Empty list is acyclic
+    }
+
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        fast_ptr = fast_ptr->next->next;  // Advance by two nodes
+        slow_ptr = slow_ptr->next;        // Advance by one node
+
+        if (fast_ptr == slow_ptr) {
+            return 1;  // Cycle detected
+        }
+    }
+
+    return 0;  // No cycle found
 }
