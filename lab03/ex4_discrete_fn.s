c07ed42bbb5a9677671f588ee2684515e80f048a
Mleon7 <2710887576@qq.com>
2023-08-19 16:25:00 +0800

    lab03 Exercise 4: Array Practice

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,6 +6,10 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    addi a0, a0, 3
+    slli a0, a0, 2  # 将a0的值乘以2的平方；类比于10进制向左移两位等于乘以10的平方
+    add a0, a0, a1
+    lw a0, 0(a0)
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
